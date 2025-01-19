dbfb17249cad1241bb741bb4d97c4cdc2bc42301
Flashl3opard <sheoreyyash@gmail.com>
2025-01-19 21:00:31 +0530

    Changes in contact us page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Image from "next/image";
 import Contact from "./(Custom Components)/contactus";
 
@@ -101,5 +102,26 @@ export default function Home() {
       </div>
       <div></div>
     </>
+=======
+import AboutUs from "./(Custom Components)/aboutus";
+import Contact from "./(Custom Components)/Contactus";
+import Navbar from "./(Custom Components)/Navbar";
+import Schedule from "./(Custom Components)/Schedule";
+import Highlights from "./(Custom Components)/Highlights";
+import Events from "./(Custom Components)/Events";
+import Footer from "./(Custom Components)/Footer";
+
+export default function Home() {
+  return (
+    <div className="p-2 m-1">
+        <Navbar/>
+        <AboutUs/>
+        <Schedule/>
+        <Highlights/>
+        <Events/>
+        <Contact/>
+        <Footer/>
+    </div>
+>>>>>>> 2b9b103b57be9f3d39ceaf87c7e8e2ba262fe2c5
   );
 }

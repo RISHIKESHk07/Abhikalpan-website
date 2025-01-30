00e35acc7bc9bd92de24199147f6de836932ba93
tanishkaa08 <162544044+tanishkaa08@users.noreply.github.com>
2025-01-30 12:02:21 +0530

    sponsor

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,20 @@ import Footer from "./(Custom Components)/Footer";
 import ToBeLaunched from "./(Custom Components)/ToBeLaunched";
 import GalleryPage from "./(Custom Components)/Gallery";
 import VideoComponent from "./(Custom Components)/Video";
+import Sponsors from "./(Custom Components)/Sponsors"
 
 export default function Home() {
   return (
     <div className="">
       <Navbar className="sticky top-0 z-50" />
       <div>
+        <Sponsors/>
+        
         <ToBeLaunched />
         <GalleryPage />
         <Contact />
         <Footer />
-        <VideoComponent />
+        
       </div>
     </div>
   );

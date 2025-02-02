a762ee666549c48327628ed181a2a23114080187
tanishkaa08 <162544044+tanishkaa08@users.noreply.github.com>
2025-02-02 18:53:56 +0530

    sponsor

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./(Custom Components)/Navbar";
 import Footer from "./(Custom Components)/Footer";
 import ToBeLaunched from "./(Custom Components)/ToBeLaunched";
 import GalleryPage from "./(Custom Components)/Gallery";
-import VideoComponent from "./(Custom Components)/Video";
 import Sponsors from "./(Custom Components)/Sponsors"
 
 export default function Home() {

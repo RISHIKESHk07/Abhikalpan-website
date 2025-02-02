da67865b482351c0de0badfd9e7bf6358cb8dcb9
Flashl3opard <sheoreyyash@gmail.com>
2025-02-02 20:00:35 +0530

    Added About route and img components related to it

---
+++ app/about/page.tsx
@@ -0,0 +1,281 @@
+import React from "react";
+import Navbar from "../(Custom Components)/Navbar";
+import Footer from "../(Custom Components)/Footer";
+
+const Abt = () => {
+  return (
+    <>
+      <div className="w-auto overflow-hidden">
+        <Navbar />
+
+        <div className="">
+          <img
+            src="/images/G1.png"
+            alt=""
+            className="absolute translate-x-[165vh] translate-y-[35vh] "
+          />
+          <img
+            src="/images/G4.png"
+            alt=""
+            className="absolute translate-y-[165vh]"
+          />
+          <img
+            src="/images/G1.png"
+            alt=""
+            className="absolute translate-x-[165vh] translate-y-[340vh]"
+          />
+          <img
+            src="/images/G4.png"
+            alt=""
+            className="absolute translate-y-[520vh]"
+          />
+        </div>
+
+        <div className="flex justify-end translate-x-[-5%] translate-y-[20%] flex-wrap">
+          <img src="/images/About2.png" alt="" className="max-w-full h-auto" />
+          <img src="/images/ABOUTUs.png" alt="" className="max-w-full h-auto" />
+        </div>
+        <div className="relative mt-[15vh]">
+          <img src="images/part.png" alt="" className="w-auto h-auto" />
+
+          <div className="absolute inset-0 flex items-center justify-center translate-x-[-30%] translate-y-[-35%] flex-wrap">
+            <img
+              src="/images/up.svg"
+              alt=""
+              className="translate-x-[174.5vh] translate-y-[-5.5vh]"
+            />
+
+            <h1 className="text-white font-genos font-normal text-[48px] translate-x-[235%]">
+              About,
+            </h1>
+            <h1 className="text-white font-aporo font-normal text-[52px] translate-x-[45%] translate-y-[15%]">
+              <br />
+              PDPM IIITDMJ
+            </h1>
+
+            <p className="text-white text-[19px] font-hellvitica font-light translate-x-[-25vh] translate-y-[18vh]">
+              Where Innovation Meets Design <br />
+              to Shape the Future of Technology!
+            </p>
+            <img
+              src="/images/logo.png"
+              alt=""
+              className="translate-x-[25vh] translate-y-[15vh]"
+            />
+            <img
+              src="/images/down.svg"
+              alt=""
+              className="translate-x-[22.5vh] translate-y-[36.5vh]"
+            />
+          </div>
+        </div>
+        <div className="text-white font-hellvitica text-[20px] translate-x-[9vh] translate-y-9">
+          <p>
+            The Indian Institute of Information Technology, Design, and <br />
+            Manufacturing (IIITDM) Jabalpur, established in 2005, is an <br />
+            Institute of National Importance under the Ministry of Education,{" "}
+            <br />
+            Government of India. The institute offers undergraduate, <br />
+            postgraduate, and doctoral programs in fields like Computer Science,{" "}
+            <br />
+            Electronics and Communication, Mechanical Engineering, and Smart{" "}
+            <br />
+            Manufacturing. Life at IIITDM Jabalpur is dynamic, with students{" "}
+            <br />
+            engaging in technical, cultural, and sports activities. Annual{" "}
+            <br />
+            events like Tarang and Abhikalpan attract talent from across the
+            country.
+          </p>
+        </div>
+        <div className="flex gap-10 translate-y-[20vh] translate-x-24 flex-wrap">
+          <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+          <img src="/images/IMG2.png" alt="" className="max-w-full h-auto" />
+          <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+        </div>
+        <div className="relative mt-[35vh]">
+          <img
+            src="images/part2.png"
+            alt=""
+            className="w-auto h-auto translate-x-[64.5vh]"
+          />
+          <div>
+            <img
+              src="/images/logo2.png"
+              alt=""
+              className="translate-x-[78vh] translate-y-[-30vh] h-[20vh]"
+            />
+
+            <div className="absolute inset-0 flex items-center justify-center translate-x-[-30%] translate-y-[-43.38%] flex-wrap">
+              <img
+                src="/images/up2.png"
+                alt=""
+                className="translate-x-[89vh] translate-y-[-26.5vh]"
+              />
+              <div className="flex justify-center items-center translate-x-[120vh] translate-y-[-20vh] flex-wrap">
+                <h1 className="text-white font-genos font-normal text-[48px] translate-x-[175%]">
+                  More About,
+                </h1>
+
+                <h1 className="text-white font-aporo font-normal text-[52px] translate-x-[45%] translate-y-[15%]">
+                  <br />
+                  ABHIKALPAN
+                </h1>
+                <p className="text-white text-[18px] font-hellvitica font-light translate-x-[-27vh] translate-y-[20vh]">
+                  Bridging Ideas and Innovation to Shape <br /> the Future of
+                  Technology and Design!
+                </p>
+              </div>
+              <img
+                src="/images/down.svg"
+                alt=""
+                className="translate-x-[-43vh] translate-y-[14.5vh] -scale-x-100"
+              />
+            </div>
+          </div>
+
+          <div className="text-white font-hellvitica text-[20px] translate-x-[-10vh] text-right -translate-y-14">
+            Abhikalpan is the annual techno-design fest of IIITDM Jabalpur,
+            <br />
+            celebrating innovation, creativity, and technical excellence. It
+            attracts <br />
+            students and professionals nationwide, offering competitions,
+            workshops, <br />
+            and exhibitions that showcase talent and explore emerging <br />
+            technologies. The fest features coding challenges, robotics and
+            design <br />
+            contests, hackathons, and expert-led seminars. Over the years,
+            Abhikalpan <br /> has become a landmark event, embodying IIITDM
+            Jabalpur's <br />
+            commitment to advancing technology and design.
+          </div>
+
+          <div className="flex gap-10 translate-y-16 translate-x-20 flex-wrap">
+            <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+            <img src="/images/IMG2.png" alt="" className="max-w-full h-auto" />
+            <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+          </div>
+
+          <div className=""></div>
+          <div className="relative mt-[15vh]">
+            <img src="images/part.png" alt="" className="w-auto h-auto" />
+
+            <div className="absolute inset-0 flex items-center justify-center translate-x-[-30%] translate-y-[-35%] flex-wrap">
+              <img
+                src="/images/up.svg"
+                alt=""
+                className="translate-x-[178vh] translate-y-[-5.5vh]"
+              />
+
+              <h1 className="text-white font-genos font-normal text-[48px] translate-x-[235%]">
+                About,
+              </h1>
+              <h1 className="text-white font-aporo font-normal text-[52px] translate-x-[25%] translate-y-[15%]">
+                <br />
+                ABHIKALPAN'S THEME
+              </h1>
+
+              <p className="text-white text-[19px] font-hellvitica font-light translate-x-[-62vh] translate-y-[18.5vh]">
+                Navigating the Evolution of Technology and <br />
+                Design, from Tradition to Tomorrow!
+              </p>
+
+              <img
+                src="/images/down.svg"
+                alt=""
+                className="translate-x-[20vh] translate-y-[36.5vh]"
+              />
+            </div>
+          </div>
+          <div className="text-white font-hellvitica text-[20px] translate-x-[9vh] translate-y-9">
+            <p>
+              Abhikalpan'25 celebrates the transformative journey of technology{" "}
+              <br />
+              and design, bridging the past, present, and future. From AI and{" "}
+              <br />
+              quantum computing to sustainable design and smart cities, <br />
+              innovation is reshaping industries and our world. This year’s fest{" "}
+              <br />
+              honors visionary thinkers across eras, reflecting on past wisdom{" "}
+              <br />
+              and future possibilities to craft a better tomorrow. Join us in{" "}
+              <br />
+              celebrating the journey of innovation that shapes the future!{" "}
+              <br />
+            </p>
+          </div>
+          <div className="flex gap-10 translate-y-16 translate-x-20 flex-wrap">
+            <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+            <img src="/images/IMG2.png" alt="" className="max-w-full h-auto" />
+            <img src="/images/IMG1.png" alt="" className="max-w-full h-auto" />
+          </div>
+          <div className="relative mt-[35vh]">
+            <img
+              src="images/part2.png"
+              alt=""
+              className="w-auto h-auto translate-x-[64.5vh]"
+            />
+            <div>
+              <div className="absolute inset-0 flex items-center justify-center translate-x-[-30%] translate-y-[-35%] flex-wrap">
+                <img
+                  src="/images/up2.png"
+                  alt=""
+                  className="translate-x-[68vh] translate-y-[-15vh]"
+                />
+                <div className="flex justify-center items-center translate-x-[115vh] translate-y-[-22vh] flex-wrap">
+                  <h1 className="text-white font-genos font-normal text-[48px] translate-x-[138%] translate-y-[18vh]">
+                    About,
+                  </h1>
+
+                  <h1 className="text-white font-aporo font-normal text-[52px] translate-x-[9%] translate-y-[95%]">
+                    <br />
+                    MEDIA OUTREACH
+                  </h1>
+                </div>
+                <img
+                  src="/images/down.svg"
+                  alt=""
+                  className="translate-x-[-22.5vh] translate-y-[26vh] -scale-x-100"
+                />
+              </div>
+            </div>
+
+            <div className="text-white font-hellvitica text-[20px] translate-x-[-10vh] text-right translate-y-0">
+              Media Outreach plays a crucial role in amplifying the impact of{" "}
+              <br />
+              events and activities at IIITDM Jabalpur, ensuring they reach a{" "}
+              <br />
+              wider audience. It involves strategically engaging with various{" "}
+              <br />
+              media platforms to promote the institute’s initiatives, <br />
+              achievements, and events like Abhikalpan, and Tarang. <br />
+            </div>
+
+            <div className="flex gap-10 translate-y-[7vh] translate-x-20 flex-wrap">
+              <img
+                src="/images/IMG1.png"
+                alt=""
+                className="max-w-full h-auto"
+              />
+              <img
+                src="/images/IMG2.png"
+                alt=""
+                className="max-w-full h-auto"
+              />
+              <img
+                src="/images/IMG1.png"
+                alt=""
+                className="max-w-full h-auto"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="h-auto  w-auto translate-y-[10vh]">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Abt;

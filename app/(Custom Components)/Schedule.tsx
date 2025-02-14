9654d1e937434650a144f695e3549d58598279b3
yashpatil641 <yashbhadane240@gmail.com>
2025-02-14 21:51:57 +0530

    fixed schedule page and updated data

--- app/(Custom Components)/Schedule.tsx
+++ app/(Custom Components)/Schedule.tsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 import React from 'react';
 import Date from '@/public/images/date_bg.svg';
@@ -14,46 +13,46 @@ export interface EventType {
 
 export const eventData: EventType[] = [
   {
-    title: "Hackathon",
-    organizer: "TPC - The programming club",
+    title: "3D Printing Workshop",
+    organizer: "CAD & 3D PRINTING CLUB",
     image: Date,
     time: "2 PM",
-    date: "5 Feb '25"
+    date: "1 March '25"
   },
   {
-    title: "Code Quest",
-    organizer: "TPC - The programming club",
+    title: "CryptoTrading Hackathon",
+    organizer: "BUSINESS AND MANAGEMENT CLUB",
+    image: Date,
+    time: "2 PM",
+    date: "1 March '25"
+  },
+  {
+    title: "Velocity Vista",
+    organizer: "AERO FABRICATION CLUB",
     image: Date,
     time: "4 PM",
-    date: "5 Feb '25"
+    date: "1 March '25"
   },
   {
-    title: "Web Development",
+    title: "CSS BattleField",
     organizer: "TPC - The programming club",
     image: Date,
-    time: "10 AM",
-    date: "6 Feb '25"
+    time: "9 AM",
+    date: "2 March '25"
   },
   {
-    title: "AI Workshop",
+    title: "E-Sports",
     organizer: "TPC - The programming club",
     image: Date,
     time: "2 PM",
-    date: "6 Feb '25"
+    date: "2 March '25"
   },
   {
-    title: "CTF Challenge",
-    organizer: "TPC - The programming club",
+    title: "CAD BattleField",
+    organizer: "CAD & 3D PRINTING CLUB",
     image: Date,
-    time: "5 PM",
-    date: "5 Feb '25"
-  },
-  {
-    title: "IoT Workshop",
-    organizer: "TPC - The programming club",
-    image: Date,
-    time: "11 AM",
-    date: "6 Feb '25"
+    time: "2 PM",
+    date: "2 March '25"
   },
   {
     title: "Machine Learning",
@@ -63,114 +62,179 @@ export const eventData: EventType[] = [
     date: "5 Feb '25"
   },
   {
-    title: "Cybersecurity",
+    title: "RC Glider Competition",
+    organizer: "AERO FABRICATION CLUB",
+    image: Date,
+    time: "11 AM",
+    date: "2 March '25"
+  },
+  {
+    title: "Robowars competition",
+    organizer: "ELECTRONICS AND ROBOTICS SOCIETY",
+    image: Date,
+    time: "11 AM",
+    date: "3 March '25"
+  },
+  {
+    title: "Innovation Design",
+    organizer: "CAD & 3D PRINTING CLUB",
+    image: Date,
+    time: "9 AM",
+    date: "3 March '25"
+  },
+  {
+    title: "Sample Competition",
+    organizer: "Sample CLUB",
+    image: Date,
+    time: "11 AM",
+    date: "2 March '25"
+  },
+  {
+    title: "The Code Arena",
     organizer: "TPC - The programming club",
     image: Date,
-    time: "1 PM",
-    date: "6 Feb '25"
+    time: "10 AM",
+    date: "3 March '25"
+  },
+  {
+    title: "EV Workshop",
+    organizer: "RACING CLUB",
+    image: Date,
+    time: "2 PM",
+    date: "3 March '25"
   }
 ];
 
-const eventData5feb = eventData.filter((event) => event.date === "5 Feb '25");
+const eventData1march = eventData.filter((event) => event.date === "1 March '25");
+const eventData2march = eventData.filter((event) => event.date === "2 March '25");
+const eventData3march = eventData.filter((event) => event.date === "3 March '25");
 
 
 const Schedule: React.FC = () => {
   return (
-    <section className="w-full py-8 relative">
-      {/* Header */}
-      <div className="text-center pb-[8vw] pl-[9vw] font-inter relative md:text-[6vw] text-[9vw]">
-          <h1 className='absolute text-white z-40'>Schedule</h1>
-          <h1 className='absolute text-[#01F8FF] blur-sm z-0'>Schedule</h1>
+    <section className="w-full py-8 relative bg-pink-500/500">
+      <div className="text-center pb-[8vw] bg-blue-300/300 flex justify-center font-inter  md:text-[6vw] text-[9vw] md:h-[10vw] h-[18vw]">
+        <h1 className='absolute text-white z-40'>Schedule</h1>
+        <h1 className='absolute text-[#01F8FF] blur-sm z-0'>Schedule</h1>
       </div>
-      {/* Dates */}
-      <div className="flex flex-col sm:flex-row justify-between md:justify-start gap-4 md:gap-[29rem] px-4 md:px-[8rem] mt-8 mb-4">
-        <p className="font-medium text-white font-genos text-3xl md:text-5xl text-center md:text-left">
-          5 Feb &apos;25
-        </p>
-        <p className="font-medium text-white font-genos text-3xl md:text-5xl text-center md:text-left sm:block hidden">
-          6 Feb &apos;25
-        </p>
-      </div>
-
-      {/* Events Grid */}
-      <div className='sm:block hidden'>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-8 px-4 md:px-[8rem] ">
-          {eventData.map((event, index) => (
+      <div className='w-full bg-green-500/500 sm:hidden flex-col h-[90vw] py-5 flex justify-between'>
+        <div className='font-genos text-center text-white text-[8vw]'>
+          1 March &apos;25
+        </div>
+        {
+          eventData1march.map((event, index) => (
             <div
               key={index}
-              className="rounded-lg flex flex-row sm:flex-row gap-2 md:gap-8 justify-start items-center md:items-start relative  "
+              className="bg-yellow-200/200 w-full h-auto flex flex-row "
             >
-              {/* Time and Image */}
-              <div className="relative w-[30%] md:w-auto md:left-0 flex justify-center md:self-start cursor-pointer bg-pink-300/300">
+              <div className='w-[20%] bg-lime-700/700 h-full flex items-center justify-center'>
                 <Image
                   src={event.image}
                   alt={`Event ${index + 1}`}
                   className=""
                 />
-                <div className="absolute inset-0 flex flex-col items-center justify-center pl-3 pt-2 text-white font-genos">
-                  <p className="text-[4.5vw]  leading-[2.5vw]">{event.time.split(" ")[0]}</p>
-                  <p className="text-base md:text-lg">PM</p>
+                <div className='h-[20vw] w-[20vw] bg-orange-400/400 absolute rounded-full flex items-center justify-center flex-col ml-3'>
+                  <p className='text-white font-genos text-[9vw] leading-[3vw] mt-[1vw]'>{event.time.split(" ")[0]}</p>
+                  <p className='text-white font-genos text-[3.5vw]'>{event.time.split(" ")[1]}</p>
                 </div>
               </div>
-
-              {/* Event Details */}
-              <div className="font-genos flex flex-col items-center md:items-start justify-center">
-                <p className="text-2xl md:text-4xl text-white text-center md:text-left">
-                  {event.title}
-                </p>
-                <p className="text-lg md:text-xl text-white text-center md:text-left">
-                  {event.organizer}
-                </p>
+              <div className='w-[75%] h-full text-white font-genos pl-[6vw] flex flex-col justify-center'>
+                <p className='text-[5vw]'>{event.title}</p>
+                <p className='text-[3vw]'>{event.organizer}</p>
               </div>
             </div>
-          ))}
-        </div>  
+          ))
+        }
       </div>
-      <div className='block sm:hidden'>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 px-4 md:px-[8rem] ">
-          {eventData5feb.map((event, index) => (
-            <div
-              key={index}
-              className="rounded-lg flex flex-row md:flex-row gap-2 md:gap-8 justify-start items-center md:items-start relative  "
-            >
-              {/* Time and Image */}
-              <div className="relative w-[30%] md:w-auto md:left-0 flex justify-center md:self-start cursor-pointer bg-pink-300/300">
-                <Image
-                  src={event.image}
-                  alt={`Event ${index + 1}`}
-                  className=""
-                />
-                <div className="absolute inset-0 flex flex-col items-center justify-center pl-3 pt-2 text-white font-genos">
-                  <p className="text-[4.5vw]  leading-[2.5vw]">{event.time.split(" ")[0]}</p>
-                  <p className="text-base md:text-lg">PM</p>
+      <div className='w-full bg-green-500/500 h-full hidden sm:flex-row sm:flex'>
+        <div className='w-1/3 bg-blue-500/500 h-full flex flex-col gap-[2vw] py-14'>
+          <div className='font-genos text-center text-white text-[4vw]'>
+            1 March &apos;25
+          </div>
+          {
+            eventData1march.map((event, index) => (
+              <div
+                key={index}
+                className="bg-yellow-200/200  w-full h-[8vw] flex flex-row"
+              >
+                <div className='w-[25%] bg-lime-700/700 h-full flex items-center justify-center'>
+                  <Image
+                    src={event.image}
+                    alt={`Event ${index + 1}`}
+                    className=""
+                  />
+                  <div className='h-[7vw] w-[7vw] bg-orange-400/400 absolute rounded-full flex items-center justify-center flex-col ml-3'>
+                    <p className='text-white font-genos text-[3vw] leading-[1vw] mt-[1vw]'>{event.time.split(" ")[0]}</p>
+                    <p className='text-white font-genos text-[1.5vw]'>{event.time.split(" ")[1]}</p>
+                  </div>
+                </div>
+                <div className='w-[75%] h-full text-white font-genos pl-[1vw]'>
+                  <p className='text-[2.2vw] leading-[3vw]'>{event.title}</p>
+                  <p className='text-[1.6vw] leading-[1.5vw]'>{event.organizer}</p>
+                </div>
+
+              </div>
+            ))
+          }
+        </div>
+        <div className='w-1/3 bg-amber-500/500 h-full flex flex-col gap-[2vw] py-14'>
+          <div className='font-genos text-center text-white text-[4vw]'>
+            2 March &apos;25
+          </div>
+          {
+            eventData2march.map((event, index) => (
+              <div
+                key={index}
+                className="bg-red-200/200 w-full h-[8vw] flex flex-row"
+              >
+                <div className='w-[25%] bg-lime-700/700 h-full flex items-center justify-center'>
+                  <Image
+                    src={event.image}
+                    alt={`Event ${index + 1}`}
+                    className=""
+                  />
+                  <div className='h-[7vw] w-[7vw] bg-orange-400/400 absolute rounded-full flex items-center justify-center flex-col ml-3'>
+                    <p className='text-white font-genos text-[3vw] leading-[1vw] mt-[1vw]'>{event.time.split(" ")[0]}</p>
+                    <p className='text-white font-genos text-[1.5vw]'>{event.time.split(" ")[1]}</p>
+                  </div>
+                </div>
+                <div className='w-[75%] h-full text-white font-genos pl-[1vw]'>
+                  <p className='text-[2.2vw] leading-[3vw]'>{event.title}</p>
+                  <p className='text-[1.6vw] leading-[1.5vw]'>{event.organizer}</p>
                 </div>
               </div>
-
-              {/* Event Details */}
-              <div className="font-genos flex flex-col items-center md:items-start justify-center">
-                <p className="text-2xl md:text-4xl text-white text-center md:text-left">
-                  {event.title}
-                </p>
-                <p className="text-lg md:text-xl text-white text-center md:text-left">
-                  {event.organizer}
-                </p>
+            ))
+          }
+        </div>
+        <div className='w-1/3 bg-blue-500/500 h-full flex flex-col gap-[2vw] py-14'>
+          <div className='font-genos text-center text-white text-[4vw]'>
+            3 March &apos;25
+          </div>
+          {
+            eventData3march.map((event, index) => (
+              <div
+                key={index}
+                className="bg-yellow-200/200 w-full h-[8vw] flex flex-row relative"
+              >
+                <div className='w-[25%] bg-lime-700/700 h-full flex items-center justify-center'>
+                  <Image
+                    src={event.image}
+                    alt={`Event ${index + 1}`}
+                    className=""
+                  />
+                  <div className='h-[7vw] w-[7vw] bg-orange-400/400 absolute rounded-full flex items-center justify-center flex-col ml-2'>
+                    <p className='text-white font-genos text-[3vw] leading-[1vw] mt-[1vw]'>{event.time.split(" ")[0]}</p>
+                    <p className='text-white font-genos text-[1.5vw]'>{event.time.split(" ")[1]}</p>
+                  </div>
+                </div>
+                <div className='w-[75%] h-full text-white font-genos pl-[1vw]'>
+                  <p className='text-[2.2vw] leading-[3vw]'>{event.title}</p>
+                  <p className='text-[1.6vw] leading-[1.5vw]'>{event.organizer}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>  
-      </div>
-      <Image
-        alt='gears'
-        src={gears}
-        className='md:w-[65vw] w-[80vw] -bottom-[20vw] absolute -right-[12vw] opacity-15'
-        />
-      <div className='md:w-[22vw] w-[33vw]  bg-blue-600/600 flex items-center justify-center absolute right-[10vw] -bottom-[8vw]'>
-        <Image
-        alt='Know More'
-        src={KnowMoreImage}
-        className='w-full'
-        />
-        <p className='absolute md:text-[2vw] text-[4vw]  text-[#FFFFFF] font-genos'>Know More</p>
+            ))
+          }
+        </div>
       </div>
     </section>
   );
